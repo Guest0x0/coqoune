9e8a2b077b30651f6af7928d19e883356e3be195
Guest0x0 <guest0x0@outlook.com>
2020-06-04 21:51:38 +0800

    add incremental result update

--- kak_util.sh
+++ kak_util.sh
@@ -5,8 +5,8 @@ function kak_refresh_processed() {
     col=${location_list[-2]}
     if [ -n "$line" -a -n "$col" ]; then
         ( printf "evaluate-commands -buffer %%opt{coqoune_buffer} %%{"
-          printf " set-option buffer coqoune_executed_highlighters %%val{timestamp}"
-          printf " 1.1,$line.$col|coqoune_executed }\n"
+          printf " set-option buffer coqoune_processed_highlighters %%val{timestamp}"
+          printf " 1.1,$line.$col|coqoune_processed }\n"
         ) | kak -p $session
     fi
 }
@@ -18,7 +18,7 @@ function kak_refresh_error() {
     e_col=$4
     if [ -n "$s_line" -a -n "$s_col" ] && [ -n "$e_line" -a -n "$e_col" ]; then
         ( printf "evaluate-commands -buffer %%opt{coqoune_buffer} %%{"
-          printf " set-option buffer coqoune_executed_highlighters %%opt{coqoune_executed_highlighters}"
+          printf " set-option buffer coqoune_processed_highlighters %%opt{coqoune_processed_highlighters}"
           printf " $s_line.$s_col,$e_line.$e_col|error"
         ) | kak -p $session
     fi
@@ -34,13 +34,28 @@ function kak_refresh_goal() {
     ) | kak -p $session
 }
 
+# $1 (optional):
+#     -incr : don't erase previous result content
 function kak_refresh_result() {
     read highlighters
-    cat - > $res_file
+    if [ "$1" = '-incr' ]; then
+        cat - | sed -n 's/\\n/\n/g; p' >>$res_file
+    else
+        cat - | sed -n 's/\\n/\n/g; p' >$res_file
+    fi
     ( printf "execute-keys -buffer '*result*' %%{"
       printf " %%|cat<space>/tmp/coqoune-$session/result<ret> }\n"
-      printf "evaluate-commands -buffer '*result' %%{"
-      printf " set-option buffer coqoune_result_highlighters %%val{timestamp} $highlighters }\n"
+      printf "evaluate-commands -buffer '*result*' %%{"
+      if [ "$1" = '-incr' ]; then
+          printf ' evaluate-commands %%sh{'
+          printf ' echo $kak_opt_coqoune_result_highlighters | ('
+          printf ' read ts highlighters;'
+          printf ' echo "set-option buffer coqoune_result_highlighters'
+          printf " %%val{timestamp} \$highlighters $highlighters\""
+          printf " ) }}\n"
+      else
+          printf " set-option buffer coqoune_result_highlighters %%val{timestamp} $highlighters }\n"
+      fi
     ) | kak -p $session
 }
 
